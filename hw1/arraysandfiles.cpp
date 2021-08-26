b16b339eaa18e7e2d2d89301c7a75bb5d6309e51
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2021-08-26 13:15:35 -0500

    sdfsdf

--- hw1/arraysandfiles.cpp
+++ hw1/arraysandfiles.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <fstream>
 #include "ArgumentManager.h"
 using namespace std;
+
+void readInputFile(string);
+void readCommandFile();
+void printResults();
+
+
 
 int main(int argc, char* argv[])
 {
 	ArgumentManager am(argc, argv);
-	const string input = am.get("input");
+	string input = am.get("input");
 	const string output = am.get("output");
 	const string command = am.get("command");
 
+	//Temp hardcoded file name
+	input = "input12.txt";
+
+	readInputFile(input);
     
+}
+
+void readInputFile(string input) {
+	ifstream inputFile(input);
+	string word;
+
+	if (inputFile) {
+		while (!inputFile.eof()) {
+			cin >> word;
+			cout << word << endl;
+		}
+	}
+
+	inputFile.close();
+}
+
+void readCommandFile() {
+
+}
+
+void printResults() {
+
 }
