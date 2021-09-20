1f1a23077e14405b5fbc39ba68e5c61a11b51e4d
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2021-09-19 20:28:11 -0500

    Continued work on lab2

--- labs/lab2/main/main.cpp
+++ labs/lab2/main/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	string input = am.get("input");
 	string output = am.get("output");
 
-	input = "input1.txt";
+	input = "input4.txt";
 	output = "ans1.txt";
 
 	LinkedList gameData;
@@ -28,17 +28,41 @@ int main(int argc, char* argv[])
 
 void readInputFile(LinkedList& gameData, string inputFile) {
 	ifstream input(inputFile);
+	string score;
 
 	if (input) {
 		while (!input.eof()) {
 			string temp = "";
 			string line = "";
+			string strScore;
+			int score;
+			string name;
+			int comma1 = -1;
+			int comma2 = -1;
 
 			getline(input, temp);
 
 			for (unsigned int i = 0; i < temp.length(); i++)
 				if (temp[i] != ' ')
 					line += temp[i];
+
+			comma1 = line.find(',');
+			comma2 = line.find(',', comma1 + 1);
+
+			if ((comma1  >= 0 && comma1 <= line.length()) && !(comma2 >= 0)) {
+
+				stringstream ss(line);
+				getline(ss, name, ',');
+				getline(ss, strScore);
+				score = stoi(strScore);
+
+				cout << "Name: " << name << ", Score:" << score << endl;
+
+				gameData.addToEnd(name, score);
+			}
+
+
+
 		}
 	}
 }
