63aad4754d37d5f824bfada8e26395c67d3bbf9f
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2021-11-21 21:10:32 -0600

    hw5

--- homework/hw5/main/btree.h
+++ homework/hw5/main/btree.h
@@ -1,11 +1,99 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 class Node
 {
-private:
-
+    int* keys;  // An array of keys
+    int t;      // Minimum degree (defines the range for number of keys)
+    Node** C; // An array of child pointers
+    int n;     // Current number of keys
+    bool leaf; // Is true when node is leaf. Otherwise false
 public:
+    Node(int _t, bool _leaf);   // Constructor
+
+    // A utility function to insert a new key in the subtree rooted with
+    // this node. The assumption is, the node must be non-full when this
+    // function is called
+    void insertNonFull(int k);
+
+    // A utility function to split the child y of this node. i is index of y in
+    // child array C[].  The Child y must be full when this function is called
+    void splitChild(int i, Node* y);
+
+    // A function to traverse all nodes in a subtree rooted with this node
+    void traverse();
+
+    // A function to search a key in the subtree rooted with this node.
+    Node* search(int k);   // returns NULL if k is not present.
+
+// Make BTree friend of this so that we can access private members of this
+// class in BTree functions
+    friend class BTree;
+};
+
+// A BTree
+class BTree
+{
+    Node* root; // Pointer to root node
+    int t;  // Minimum degree
+public:
+    // Constructor (Initializes tree as empty)
+    BTree(int _t)
+    {
+        root = NULL;  t = _t;
+    }
+
+    // function to traverse the tree
+    void traverse()
+    {
+        if (root != NULL) root->traverse();
+    }
+
+    // function to search a key in this tree
+    Node* search(int k)
+    {
+        return (root == NULL) ? NULL : root->search(k);
+    }
+
+    // The main function that inserts a new key in this B-Tree
+    void insert(int k)
+    {
+        // If tree is empty
+        if (root == NULL)
+        {
+            // Allocate memory for root
+            root = new Node(t, true);
+            root->keys[0] = k;  // Insert key
+            root->n = 1;  // Update number of keys in root
+        }
+        else // If tree is not empty
+        {
+            // If root is full, then tree grows in height
+            if (root->n == 2 * t - 1)
+            {
+                // Allocate memory for new root
+                Node* s = new Node(t, false);
+
+                // Make old root as child of new root
+                s->C[0] = root;
+
+                // Split the old root and move 1 key to the new root
+                s->splitChild(0, root);
+
+                // New root has two children now.  Decide which of the
+                // two children is going to have new key
+                int i = 0;
+                if (s->keys[0] < k)
+                    i++;
+                s->C[i]->insertNonFull(k);
+
+                // Change root
+                root = s;
+            }
+            else  // If root is not full, call insertNonFull for root
+                root->insertNonFull(k);
+        }
+    }
 };
--- homework/hw5/main/main.cpp
+++ homework/hw5/main/main.cpp
@@ -2,9 +2,12 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 #include "ArgumentManager.h"
 #include "btree.h"
 using namespace std;
+
+void ReadCommandFile(vector<string>&, int&, string);
 
 int main(int argc, char* argv[])
 {
@@ -17,5 +20,27 @@ int main(int argc, char* argv[])
 	command = "command51.txt";
 	output = "output51.txt";
 
+	int degree = 0;
+	vector<string> commands;
 
+	ReadCommandFile(commands, degree, command);
+
+}
+
+void ReadCommandFile(vector<string>& commands, int& degree, string commandFile)
+{
+	ifstream command(commandFile);
+	string line;
+	getline(command, line);
+
+	degree = stoi(line.substr(line.find('=') + 1));
+
+	while (!command.eof())
+	{
+		string temp;
+		getline(command, temp);
+		commands.push_back(temp);
+	}
+
+	command.close();
 }
